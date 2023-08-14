e9424c3b304da7de2cb3e26d755d7662b8082ce1
Falcon <jfalcon@D01DEVJfalcon01.local>
2023-08-14 09:56:26 -0500

    additional linting

--- index.js
+++ index.js
@@ -6,70 +6,70 @@ dotenv.config();
 // site id = l0XScfRd
 
 const updateJwplayerMedia = async (siteId, mediaId, customParams) => {
-    const response = await axios({
-        method: "PATCH",
-        url: `https://api.jwplayer.com/v2/sites/${siteId}/media/${mediaId}/`,
-        headers: {
-            accept: "application/json",
-            'content-type': 'application/json',
-            Authorization: process.env.JWPLAYER_SECRET
+  const response = await axios({
+    method: "PATCH",
+    url: `https://api.jwplayer.com/v2/sites/${siteId}/media/${mediaId}/`,
+    headers: {
+      accept: "application/json",
+      "content-type": "application/json",
+      Authorization: process.env.JWPLAYER_SECRET,
+    },
+    data: {
+      metadata: {
+        custom_params: {
+          status: customParams.status,
+          contentType: customParams.contentType,
+          free: customParams.free,
+          livestream_channel_id: customParams.livestream_channel_id,
+          requires_authentication: customParams.requires_authentication,
         },
-        data: {
-            metadata: {
-                custom_params: {
-                    status: customParams.status,
-                    contentType: customParams.contentType,
-                    free: customParams.free,
-                    livestream_channel_id: customParams.livestream_channel_id,
-                    requires_authentication: customParams.requires_authentication
-                }
-            }
-        }
+      },
+    },
+  })
+    .then((response) => {
+      return response.date;
     })
-        .then((response) => {
-            return response.date;
-        })
-        .catch((error) => {
-            if (error.response) {
-                return {
-                    error: {
-                        status: error.response.status,
-                        data: error.response.data,
-                    },
-                };
-            } else {
-                return "Error", error.message;
-            }
-        });
-    return response;
-}
+    .catch((error) => {
+      if (error.response) {
+        return {
+          error: {
+            status: error.response.status,
+            data: error.response.data,
+          },
+        };
+      } else {
+        return "Error", error.message;
+      }
+    });
+  return response;
+};
 
 const getJwplayerMedia = async (siteId, mediaId) => {
-    const response = await axios({
-        method: "get",
-        url: `https://api.jwplayer.com/v2/sites/${siteId}/media/${mediaId}/`,
-        headers: {
-            accept: "application/json",
-            Authorization: process.env.JWPLAYER_SECRET
-        },
+  const response = await axios({
+    method: "get",
+    url: `https://api.jwplayer.com/v2/sites/${siteId}/media/${mediaId}/`,
+    headers: {
+      accept: "application/json",
+      Authorization: process.env.JWPLAYER_SECRET,
+    },
+  })
+    .then((response) => {
+      return response.data;
     })
-        .then((response) => {
-            return response.data;
-        })
-        .catch((error) => {
-            if (error.response) {
-                return {
-                    error: {
-                        status: error.response.status,
-                        data: error.response.data,
-                    },
-                };
-            } else {
-                return "Error", error.message;
-            }
-        });
-    return response;
-}
+    .catch((error) => {
+      if (error.response) {
+        return {
+          error: {
+            status: error.response.status,
+            data: error.response.data,
+          },
+        };
+      } else {
+        return "Error", error.message;
+      }
+    });
+  return response;
+};
 
 const isEmpty = (obj) => {
   for (const prop in obj) {
@@ -78,28 +78,35 @@ const isEmpty = (obj) => {
     }
   }
   return true;
-}
+};
 
 const updateMedia = async (event) => {
-    let media = await getJwplayerMedia(event.site_id, event.media_id);
-    console.log(media);
-    if (isEmpty(media.metadata.custom_params) || media.metadata.custom_params.status === "VOD") {
-        console.log('no data')
-        return
-    }
-    let customParams = media.metadata.custom_params;
-    customParams.status = "VOD";
-    let response = await updateJwplayerMedia(event.site_id, event.media_id, customParams)
-    console.log(response);
-    return response
-}
+  let media = await getJwplayerMedia(event.site_id, event.media_id);
+  console.log(media);
+  if (
+    isEmpty(media.metadata.custom_params) ||
+    media.metadata.custom_params.status === "VOD"
+  ) {
+    console.log("no data");
+    return;
+  }
+  let customParams = media.metadata.custom_params;
+  customParams.status = "VOD";
+  let response = await updateJwplayerMedia(
+    event.site_id,
+    event.media_id,
+    customParams
+  );
+  console.log(response);
+  return response;
+};
 
 export const handler = async (event, context) => {
-    if (event === "" || event === undefined) {
-        return {
-            "message": "invalid event"
-        }
-    }
-    console.log(event);
-    return updateMedia(event);
+  if (event === "" || event === undefined) {
+    return {
+      message: "invalid event",
+    };
+  }
+  console.log(event);
+  return updateMedia(event);
 };
